36bdf315b0dc3c808322695b8c56ef12cc20e735
Kendall Weihe <kendallweihe@gmail.com>
2019-11-03 15:04:57 -0500

    basic hellow world flask api, plus actual json data, plus the *start* to a Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM ubuntu
+
+RUN apt-get update -y && \
+    apt-get install -y python3-pip python-dev
+
+COPY ./requirements.txt /app/requirements.txt
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+COPY . /app
+
+EXPOSE 5000
+
+ENTRYPOINT [ "python3" ]
+
+CMD [ "main.py" ]
---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import json
+
+app_name = "kendalls-flask-app"
+app = Flask(app_name)
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+@app.route("/dummy_data/<name>")
+def get_dummy_data(name):
+    f = open("dummy_data/{0}.json".format(name), "r")
+    return jsonify(json.loads(f.read()))
+
+if __name__ == '__main__':
+    app.run()
